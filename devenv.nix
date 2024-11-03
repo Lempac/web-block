e81f6dd84df3d4126db5210e79d64c7e5745eddc
Lempac <brunis.biksis@gmail.com>
2024-11-03 19:41:48 +0200

    Nix and lock files

--- devenv.nix
+++ devenv.nix
@@ -1,27 +1,26 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  #dotenv.enable = true;
-
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/languages/
+#    dotenv.enable = true;
     languages.php.enable = true;
     languages.php.version = "8.3";
     languages.php.extensions = [ "xdebug" "pdo_mysql" ];
-    languages.php.ini = ''xdebug.mode = debug
+    languages.php.ini = ''
+        xdebug.mode = debug
         xdebug.discover_client_host = 1
         xdebug.client_host = 127.0.0.1
     '';
     languages.javascript.enable = true;
     languages.javascript.package = pkgs.nodejs_20;
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
 
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
+    processes = {
+        vite.exec = "npm run dev";
+        php-serve.exec = "sudo php artisan serve --port=80";
+        php-queue.exec = "php artisan queue:work";
+        php-reverb.exec = "php artisan reverb:start";
+    };
+
+    services.mailpit.enable = false;
     services.mysql.enable = true;
     services.mysql.ensureUsers = [
         {
@@ -32,25 +31,4 @@
             };
         }
     ];
-
-  # https://devenv.sh/scripts/
-#  scripts.hello.exec = ''
-#    echo hello from $GREET
-#  '';
-
-#  enterShell = ''
-#    hello
-#    git --version
-#  '';
-
-  # https://devenv.sh/tests/
-#  enterTest = ''
-#    echo "Running tests"
-#    git --version | grep --color=auto "${pkgs.git.version}"
-#  '';
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
