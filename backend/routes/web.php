cab96e4ec54fea67fe393f8ac070378544be19c7
Lempac <brunis.biksis@gmail.com>
2025-04-11 15:24:21 +0300

    lock files and workflows

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return Storage::url(Storage::disk('public')->get('Lempac'));
-    return ['Laravel' => app()->version()];
+    return ['Laravel' => app()->version(), 'App name' => env('APP_NAME')];
 });
 
 require __DIR__.'/auth.php';
